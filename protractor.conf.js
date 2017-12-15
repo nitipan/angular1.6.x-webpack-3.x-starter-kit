a4a38508c27af6a06d150ba89f6305f9bf411261
Nitipan Pompan <nitipan.p@hotmail.com>
2017-12-15 18:29:36 +0700

    added e2e testing

---
+++ protractor.conf.js
@@ -0,0 +1,10 @@
+exports.config = {
+    framework: 'jasmine',
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    specs: ['e2e/*.spec.js'],
+    directConnect: true,
+    multiCapabilities: [{
+        'browserName': 'chrome'
+    }]
+
+}
